52958a94f2155c5a0f5cd552964fd2fe356129fc
damong2 <bbhbee1002@gmail.com>
2026-04-28 16:26:17 +0900

    0428_v1
    영상, sub3-1,3-3 제외 취합본

---
+++ assets/javascript/main_swiper.js
@@ -0,0 +1,54 @@
+//header scroll
+let lastScrollTop = 0;
+window.addEventListener("scroll", function () {
+  if (window.innerWidth <= 768) return;
+
+  let scrollTop = window.scrollY + window.innerHeight / 2;
+  let scrollTop2 = window.scrollY;
+
+  if (scrollTop2 > lastScrollTop) {
+    document.querySelector("#header").classList.add("hide");
+  } else {
+    document.querySelector("#header").classList.remove("hide");
+  }
+  lastScrollTop = scrollTop2;
+});
+
+// 모바일 메뉴
+const button = document.querySelector("#mNav button");
+const nav = document.querySelector("#mNav nav");
+
+button.addEventListener("click", () => {
+  nav.classList.toggle("dropdown");
+});
+
+const menu = document.querySelectorAll(".mNav_nav > ul > li");
+
+menu.forEach(function (menuList) {
+  menuList.addEventListener("click", function () {
+    const subMenu = this.querySelector("ul");
+
+    if (subMenu.style.height === "120px") {
+      subMenu.style.height = "0px";
+    } else {
+      subMenu.style.height = "120px";
+    }
+  });
+});
+
+//1920px 메뉴
+const navList = document.querySelector("#header .lnb > ul");
+
+navList.addEventListener("mouseover", function () {
+  navList.querySelectorAll(".submenu2").forEach(function (sub) {
+    sub.style.height = "200px";
+  });
+  document.querySelector("#header").classList.add("on");
+});
+
+navList.addEventListener("mouseout", function () {
+  navList.querySelectorAll(".submenu2").forEach(function (sub) {
+    sub.style.height = "0";
+  });
+  document.querySelector("#header").classList.remove("on");
+});
